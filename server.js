94d3986c7039068b2065c4e305f3f6573f9b2337
dekelPK <dekelkartel@gmail.com>
2024-08-24 18:08:44 +0300

    שינוי משתנה mongo

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // חיבור ל-MongoDB
-mongoose.connect('mongodb://localhost:27017/mycourses')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
